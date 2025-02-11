a5a4093251ab2d0417671e13cdfea43cbe580383
pranay3108 <pranayhattarkal06@gmail.com>
2025-02-11 20:54:46 +0530

    updated 11-02-2025

---
+++ Master Setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This is a basic bash script
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+
+ansible --version
+
+
+sudo systemctl status jenkins
---
+++ README.txt
@@ -0,0 +1,11 @@
+git clone your repo
+
+chmod +x installjenkins.sh
+
+./installjenkins.sh
+
+sudo systemctl status jenkins
+
+#on Slave 
+install Java
+
